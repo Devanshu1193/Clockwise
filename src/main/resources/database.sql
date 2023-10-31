521026e6673b5ad4133417527881e64f372ac7bf
SantioMC <admin@santio.me>
2023-10-31 12:39:55 -0400

    Create proper tables for database

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,3 +1,16 @@
-CREATE TABLE test(
-    a INT PRIMARY KEY AUTO_INCREMENT
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE times(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    time_started INT NOT NULL,
+    time_stopped INT NOT NULL,
+    FOREIGN KEY user_id
+    REFERENCES users.id;
 );
